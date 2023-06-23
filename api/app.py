8b9745a5e8c4f749ea443281d9ec6589f371c016
Rett Graham <73906088+rettag@users.noreply.github.com>
2023-06-23 04:41:01 -0400

    Add files via upload

--- api/app.py
+++ api/app.py
@@ -7,7 +7,6 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 
 from nba_api.live.nba.endpoints import scoreboard
 from nba_api.stats.endpoints import TeamDashboardByGeneralSplits
@@ -15,8 +14,55 @@ from nba_api.stats.endpoints import TeamDashboardByGeneralSplits
 app = Flask(__name__)
 CORS(app)
 
-def probability(team1, team2, avg_table, lr):
-    
+# Return The Team Abbreviation
+def get_team_abv(team):
+    teams = {'Atlanta Hawks': 'ATL', 'Boston Celtics': 'BOS', 'Brooklyn Nets': 'BKN', 'Charlotte Hornets': 'CHA',
+        'Chicago Bulls': 'CHI', 'Cleveland Cavaliers': 'CLE', 'Dallas Mavericks': 'DAL', 'Denver Nuggets': 'DEN', 
+        'Detroit Pistons': 'DET', 'Golden State Warriors': 'GSW', 'Houston Rockets': 'HOU', 'Indiana Pacers': 'IND', 
+        'Los Angeles Clippers': 'LAC', 'Los Angeles Lakers': 'LAL', 'Memphis Grizzlies': 'MEM', 'Miami Heat': 'MIA', 
+        'Milwaukee Bucks': 'MIL', 'Minnesota Timberwolves': 'MIN', 'New Orleans Pelicans': 'NOP', 
+        'New York Knicks': 'NYK', 'Oklahoma City Thunder': 'OKC', 'Orlando Magic': 'ORL', 'Philadelphia 76ers': 'PHI', 
+        'Phoenix Suns': 'PHX', 'Portland Trail Blazers': 'POR', 'Sacramento Kings': 'SAC', 'San Antonio Spurs': 'SAS', 
+        'Toronto Raptors': 'TOR', 'Utah Jazz': 'UTA', 'Washington Wizards': 'WAS'}
+
+    return teams[team]
+
+# Returns Single Game Dates 
+def get_game_date(game_date):
+    weekday, month_and_day, year = game_date.split(',')
+    month_and_day = month_and_day.lstrip()
+    year = year.rstrip()
+    date = "{},{}".format(month_and_day, year)
+    return date
+
+# Returns Relavent Dates
+def dates():
+    month_dict = {'01': 'january', '02': 'february', '03': 'march', '04': 'april', '05': 'may', '06': 'june', 
+        '07': 'july', '08': 'august', '09': 'september', '10': 'october', '11': 'november', '12': 'december'}
+
+    todays_date = datetime.now().date()
+    year, month, day = str(todays_date).split('-')
+
+    prev_year = year
+    prev_month = int(month) - 1
+
+    if (prev_month < 10):
+        prev_month = "0" + str(prev_month)
+    prev_month = str(prev_month)
+
+    month_name = month_dict[month]
+    prev_month_name = month_dict[prev_month]
+
+    if int(month) > 9:
+        year = int(year) + 1
+
+    if int(month) == 1:
+        prev_year -= 1
+
+    return year, month, day, month_name, prev_month_name, prev_year
+
+# Predicts The Probabiliy For A Game
+def probability(team1, team2, team1_score, team2_score, avg_table, lr):
     home_stats = []
     away_stats = []
 
@@ -31,66 +77,59 @@ def probability(team1, team2, avg_table, lr):
             away_stats = [cols[21].text, cols[18].text, cols[19].text, cols[20].text, cols[15].text, cols[8].text]
 
     df = pd.DataFrame({'TOV': home_stats[0], 'AST': home_stats[1], 'STL': home_stats[2], 'BLK': home_stats[3], 'OREB': home_stats[4], 'FG3_PCT': home_stats[5],
-                        'OPP_TOV': away_stats[0], 'OPP_OREB': away_stats[4], 'OPP_AST': away_stats[1], 'OPP_STL': away_stats[2], 'OPP_FG3_PCT': away_stats[5], 'OPP_BLK': away_stats[3]}, index=[1])
+                       'OPP_TOV': away_stats[0], 'OPP_OREB': away_stats[4], 'OPP_AST': away_stats[1], 'OPP_STL': away_stats[2], 'OPP_FG3_PCT': away_stats[5], 'OPP_BLK': away_stats[3]}, index=[1])
 
     proba = lr.predict_proba(df)
 
     print(proba)
-    return proba[0][0], proba[0][1]
+    team2_win_chance = proba[0][1]
+    team1_win_chance = proba[0][0]
 
+    if (team2_win_chance > team1_win_chance):
+            team2_win_chance = team2_win_chance * 100
+            team2_win_chance = math.ceil(team2_win_chance)
+
+            team1_win_chance = team1_win_chance * 100
+            team1_win_chance = math.floor(team1_win_chance)
+    else:
+        team2_win_chance = team2_win_chance * 100
+        team2_win_chance = math.floor(team2_win_chance)
+
+        team1_win_chance = team1_win_chance * 100
+        team1_win_chance = math.ceil(team1_win_chance)
+
+    team1_win = True
+    team2_win = True
+
+    if (team1_score > team2_score):
+        team1_win = True
+        team2_win = False
+    else:
+        team1_win = False
+        team2_win = True
+        
+    return team1_win_chance, team2_win_chance, team1_win, team2_win, team1_score, team2_score
+
+# Route For All Games
 @app.route('/', methods=['GET'])
 def get_games():
+    print("Predicting Games...")
+    
+    # Open The Prediction Model
     lr = joblib.load('logistic_regression.pkl')
-    today = datetime.now().date()
-    print("Running route / ")
 
-    day_of_week = today.weekday()
+    # Get Dates
+    year, month, day, month_name, prev_month_name, prev_year = dates()
 
-    teams = {'Atlanta Hawks': 'ATL', 'Boston Celtics': 'BOS', 'Brooklyn Nets': 'BKN', 'Charlotte Hornets': 'CHA', 
-         'Chicago Bulls': 'CHI', 'Cleveland Cavaliers': 'CLE', 'Dallas Mavericks': 'DAL', 'Denver Nuggets': 'DEN', 
-         'Detroit Pistons': 'DET', 'Golden State Warriors': 'GSW', 'Houston Rockets': 'HOU', 'Indiana Pacers': 'IND', 
-         'Los Angeles Clippers': 'LAC', 'Los Angeles Lakers': 'LAL', 'Memphis Grizzlies': 'MEM', 'Miami Heat': 'MIA', 
-         'Milwaukee Bucks': 'MIL', 'Minnesota Timberwolves': 'MIN', 'New Orleans Pelicans': 'NOP', 
-         'New York Knicks': 'NYK', 'Oklahoma City Thunder': 'OKC', 'Orlando Magic': 'ORL', 'Philadelphia 76ers': 'PHI', 
-         'Phoenix Suns': 'PHX', 'Portland Trail Blazers': 'POR', 'Sacramento Kings': 'SAC', 'San Antonio Spurs': 'SAS', 
-         'Toronto Raptors': 'TOR', 'Utah Jazz': 'UTA', 'Washington Wizards': 'WAS'}
+    # ***********Changing To Test************
+
+    prev_month_name = 'february'
+    month_name = 'march'
+
+    #***********************************
 
 
-    month_dict = {'01': 'january', '02': 'february', '03': 'march', '04': 'april', '05': 'may', '06': 'june', 
-                '07': 'july', '08': 'august', '09': 'september', '10': 'october', '11': 'november', '12': 'december'}
-
-    month_dict_abv = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun', 
-                '07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'}
-
-    day_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    day_name = day_names[day_of_week]
-
-
-    year, month, day = str(today).split('-')
-
-    prev_year = year
-    prev_month = int(month) - 1
-
-    if (prev_month < 10):
-        prev_month = "0" + str(prev_month)
-
-    prev_month = str(prev_month)
-
-    month_name = month_dict[month]
-    month_abv = month_dict_abv[month]
-
-    prev_month_name = month_dict[prev_month]
-    prev_month_abv = month_dict_abv[prev_month]
-
-
-    full = "{}, {} {}, {}".format(day_name, month_abv, day, year)
-
-    if int(month) > 9:
-        year = int(year) + 1
-
-    if int(month) == 1:
-        prev_year -= 1
-
+    # URLs To Scrap
     url = 'https://www.basketball-reference.com/leagues/NBA_{}_games-{}.html'.format(year, month_name)
     url2 = 'https://www.basketball-reference.com/leagues/NBA_{}_games-{}.html'.format(prev_year, prev_month_name)
 
@@ -103,101 +142,59 @@ def get_games():
     soup2 = BeautifulSoup(response2.content, 'html.parser')
     table2 = soup2.find('table', id='schedule')
 
-    # Average Team Stats (
+    # Average Team Stats
     url1 = f"https://www.basketball-reference.com/leagues/NBA_2023.html"
     response1 = requests.get(url1)
     soup1 = BeautifulSoup(response1.content, "html.parser")
 
     team_stats_table = soup1.find("table", id="per_game-team")
     avg_table = team_stats_table.tbody.find_all("tr")
-    # )
 
+    # Find & Predict Games
     upcoming_games = []
     today_games = []
     past_games = []
 
-    # Upcoming Games and Today Games
+    # Find Upcoming Games & Today Games
     count_games = 0
-    print(table)
+
     for row in table.find_all('tr')[1:]:
         if len(row.find_all('th')) > 1:
             continue
         if count_games == 50:
             break
 
+        # Get Dates
         date = row.find_all('th')[0].text
-
         days = date.split(',')[1].split()[1]
-
-        weekday, month_and_day, year = date.split(',')
-        month_and_day = month_and_day.lstrip()
-        year = year.rstrip()
-        date = "{},{}".format(month_and_day, year)
-
+        date = get_game_date(date)
 
         cols = row.find_all('td')
-
-        team2_win_chance, team1_win_chance = probability(cols[1].text, cols[3].text, avg_table, lr)
-
-        if (team2_win_chance > team1_win_chance):
-            team2_win_chance = team2_win_chance * 100
-            team2_win_chance = math.ceil(team2_win_chance)
-
-            team1_win_chance = team1_win_chance * 100
-            team1_win_chance = math.floor(team1_win_chance)
-        else:
-            team2_win_chance = team2_win_chance * 100
-            team2_win_chance = math.floor(team2_win_chance)
-
-            team1_win_chance = team1_win_chance * 100
-            team1_win_chance = math.ceil(team1_win_chance)
-
+        
+        # Predict Game
+        team1_win_chance, team2_win_chance, team1_win, team2_win, team1_score, team2_score = probability(cols[1].text, cols[3].text, cols[4].text, cols[2].text, avg_table, lr)
+        
+        # Add Prediction
+        game_dict = {
+            'team1_win_chance': team1_win_chance,
+            'team2_win_chance': team2_win_chance,
+            'date': date,
+            'team1': cols[1].text,
+            'team1_abv': get_team_abv(cols[1].text),
+            'team2': cols[3].text,
+            'team2_abv': get_team_abv(cols[3].text),
+            'home': cols[3].text,
+            'away': cols[1].text,
+        }
+        
         if (int(days) < int(day)):
-            team1_win = True
-            team2_win = True
-            team1_score = int(cols[4].text)
-            team2_score = int(cols[2].text)
-
-            if (team1_score > team2_score):
-                team1_win = True
-                team2_win = False
-            else:
-                team1_win = False
-                team2_win = True
-
-
-            game_dict = {
-                'team1_win_chance': team1_win_chance,
-                'team2_win_chance': team2_win_chance,
-                'date': date,
-                'team1': cols[1].text,
-                'team1_abv': teams[cols[1].text],
-                'team2': cols[3].text,
-                'team2_abv': teams[cols[3].text],
-                'home': cols[3].text,
-                'away': cols[1].text,
-                'is_team1_win': team1_win,
-                'is_team2_win': team2_win,
-                'team1_score': team1_score,
-                'team2_score': team2_score,
-            }
+            game_dict['is_team1_win'] = team1_win
+            game_dict['is_team2_win'] = team2_win
+            game_dict['team1_score'] = team1_score
+            game_dict['team2_score'] = team2_score
 
             past_games.append(game_dict)
-
         else:
-            game_dict = {
-                'team1_win_chance': team1_win_chance,
-                'team2_win_chance': team2_win_chance,
-                'date': date,
-                'team1': cols[1].text,
-                'team1_abv': teams[cols[1].text],
-                'team2': cols[3].text,
-                'team2_abv': teams[cols[3].text],
-                'home': cols[3].text,
-                'away': cols[1].text,
-            }
-
-
             if (int(days) == int(day)):
                 today_games.append(game_dict)
             else:
@@ -205,58 +202,30 @@ def get_games():
 
         count_games += 1
 
-    # Past Games
+    # Find Past Games
     for row in table2.find_all('tr')[1:]:
         if len(row.find_all('th')) > 1:
             continue
 
+        # Get Dates
         date = row.find_all('th')[0].text
-
         days = date.split(',')[1].split()[1]
-
-        weekday, month_and_day, year = date.split(',')
-        month_and_day = month_and_day.lstrip()
-        year = year.rstrip()
-        date = "{},{}".format(month_and_day, year)
+        date = get_game_date(date)
 
         cols = row.find_all('td')
 
-        team2_win_chance, team1_win_chance = probability(cols[1].text, cols[3].text, avg_table, lr)
+        # Predict Game
+        team1_win_chance, team2_win_chance, team1_win, team2_win, team1_score, team2_score = probability(cols[1].text, cols[3].text, cols[4].text, cols[2].text, avg_table, lr)
 
-        if (team2_win_chance > team1_win_chance):
-            team2_win_chance = team2_win_chance * 100
-            team2_win_chance = math.ceil(team2_win_chance)
-
-            team1_win_chance = team1_win_chance * 100
-            team1_win_chance = math.floor(team1_win_chance)
-        else:
-            team2_win_chance = team2_win_chance * 100
-            team2_win_chance = math.floor(team2_win_chance)
-
-            team1_win_chance = team1_win_chance * 100
-            team1_win_chance = math.ceil(team1_win_chance)
-
-        team1_win = True
-        team2_win = True
-        team1_score = int(cols[4].text)
-        team2_score = int(cols[2].text)
-
-        if (team1_score > team2_score):
-            team1_win = True
-            team2_win = False
-        else:
-            team1_win = False
-            team2_win = True
-
-
+        # Add Prediction
         game_dict = {
             'team1_win_chance': team1_win_chance,
             'team2_win_chance': team2_win_chance,
             'date': date,
             'team1': cols[1].text,
-            'team1_abv': teams[cols[1].text],
+            'team1_abv': get_team_abv(cols[1].text),
             'team2': cols[3].text,
-            'team2_abv': teams[cols[3].text],
+            'team2_abv': get_team_abv(cols[3].text),
             'home': cols[3].text,
             'away': cols[1].text,
             'is_team1_win': team1_win,
@@ -267,8 +236,10 @@ def get_games():
 
         past_games.append(game_dict)
 
-
+    # Add All Predictions
     content = {'upcoming_games': upcoming_games, 'today_games': today_games, 'past_games': past_games}
+
+    print("Finished Predicting Games")
     return jsonify(content)
 
 
@@ -278,7 +249,6 @@ def games_update():
     games = scoreboard.ScoreBoard()
     games = games.get_dict()
 
-    print(games)
     games_info = []
     for game in games["scoreboard"]["games"]:
         game_info = {
--- model/model.py
+++ model/model.py
@@ -1,10 +1,8 @@
 from nba_api.stats.endpoints import leaguegamefinder
 from sklearn.linear_model import LogisticRegression
-from sklearn import svm
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.metrics import accuracy_score
-import sys
 
 from nba_api.stats.endpoints import leaguestandingsv3
 from nba_api.stats.endpoints import TeamDashboardByGeneralSplits
@@ -31,7 +29,7 @@ games = games.groupby("GAME_ID", group_keys=False).apply(add_away)
 
 games = games.dropna()
 del games['GAME_ID']
-#print(games)
+print(games)
 
 # Create separate dataframes for input features and target variable
 X = games.drop('WL', axis=1)
@@ -40,141 +38,16 @@ y = games['WL'].map({'W': 1, 'L': 0}).astype(int)
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
 
-# Create an instance of a Support Vector Machine (SVM) to classify data as win/loss
-classifier = svm.SVC(kernel='linear')
-classifier.fit(X_train, y_train)
-
-Ypred = classifier.predict(X=X_test)
-acc = accuracy_score(y_test, Ypred)
-print("Linear Classification (SVM) Accuracy:", acc)
-joblib.dump(classifier, 'svm.pkl')
-
-
-# Create and train a logistic regression model
 lr = LogisticRegression()
 lr.fit(X_train, y_train)
 
-
-# Calculate the accuracy of the model
 y_pred = lr.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print("Logistic Accuracy:", accuracy)
+
+# Accuracy 86%!
+print("Accuracy:", accuracy)
 
 proba = lr.predict_proba(X_test)
 
+# Save As A File
 joblib.dump(lr, 'logistic_regression.pkl')
-
-# # Put results into database
-# team_ids = {
-#     "ATL": 1610612737,
-#     "BOS": 1610612738,
-#     "BKN": 1610612751,
-#     "CHA": 1610612766,
-#     "CHI": 1610612741,
-#     "CLE": 1610612739,
-#     "DAL": 1610612742,
-#     "DEN": 1610612743,
-#     "DET": 1610612765,
-#     "GSW": 1610612744,
-#     "HOU": 1610612745,
-#     "IND": 1610612754,
-#     "LAC": 1610612746,
-#     "LAL": 1610612747,
-#     "MEM": 1610612763,
-#     "MIA": 1610612748,
-#     "MIL": 1610612749,
-#     "MIN": 1610612750,
-#     "NOP": 1610612740,
-#     "NYK": 1610612752,
-#     "OKC": 1610612760,
-#     "ORL": 1610612753,
-#     "PHI": 1610612755,
-#     "PHX": 1610612756,
-#     "POR": 1610612757,
-#     "SAC": 1610612758,
-#     "SAS": 1610612759,
-#     "TOR": 1610612761,
-#     "UTA": 1610612762,
-#     "WAS": 1610612764
-# }
-
-# conn = sqlite3.connect('games.db')
-# cursor = conn.cursor()
-
-# cursor.execute('''
-#         CREATE TABLE IF NOT EXISTS games (
-#             team1 text,
-#             team2 text,
-#             win_percent_team1 float,
-#             win_percent_team2 float
-#         )
-#     ''')
-
-# cursor.close()
-
-# for team_id1 in team_ids:
-#     for team_id2 in team_ids:
-
-#         if team_id1 == team_id2:
-#             continue
-
-        
-#         season_year = '2021-22'
-
-#         # Retrieve the team dashboard stats for game splits
-#         team_dashboard = TeamDashboardByGeneralSplits(team_id=team_ids[team_id1], season=season_year)
-
-#         # Get the data frame for the team stats
-#         team_stats = team_dashboard.get_data_frames()[0]
-#         team_stats = team_stats[['TOV', 'AST', 'STL', 'BLK', 'OREB', 'FG3_PCT']]
-#         team_stats = team_stats / 82
-#         team_stats["FG3_PCT"] = team_stats["FG3_PCT"] * 82
-#         # Print the data frame
-#         print(team_stats.head())
-
-#         home_stats = team_stats
-
-#         # Retrieve the team dashboard stats for game splits
-#         team_dashboard = TeamDashboardByGeneralSplits(team_id=team_ids[team_id2], season=season_year)
-
-#         # Get the data frame for the team stats
-#         team_stats = team_dashboard.get_data_frames()[0]
-#         team_stats = team_stats[['TOV', 'OREB', 'AST', 'STL', 'FG3_PCT', 'BLK']]
-#         team_stats = team_stats / 82
-#         team_stats["FG3_PCT"] = team_stats["FG3_PCT"] * 82
-#         # Print the data frame
-#         print(team_stats.head())
-
-#         away_stats = team_stats
-
-        # home_stats["OPP_TOV"] = away_stats["TOV"]
-        # home_stats["OPP_OREB"] = away_stats["OREB"]
-        # home_stats["OPP_AST"] = away_stats["AST"]
-        # home_stats["OPP_STL"] = away_stats["STL"]
-        # home_stats["OPP_FG3_PCT"] = away_stats["FG3_PCT"]
-        # home_stats["OPP_BLK"] = away_stats["BLK"]
-
-#         print(home_stats)
-
-#         y_pred = lr.predict(home_stats)
-#         proba = lr.predict_proba(home_stats)
-
-#         team2_win_percent = proba[0]
-#         team1_win_percent = proba[1]
-
-#         insert = conn.cursor()
-#         insert.execute('INSERT INTO players (team1, team2, win_percent_team1, win_percent_team2) VALUES (?, ?, ?, ?)', (team_id1, team_id2, team1_win_percent, team2_win_percent))
-#         insert.close()
-#         print(y_pred, proba)
-
-# conn.commit()
-# conn.close()
-
-# # # Today's Score Board
-# # games = scoreboard.ScoreBoard()
-
-# # # json
-# # games.get_json()
-
-# # # dictionary
-# # games.get_dict()
